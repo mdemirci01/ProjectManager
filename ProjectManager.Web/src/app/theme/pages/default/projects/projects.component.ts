0a7b6a1d10a3d529fc96bdb48d874d30695ad0b3
Murat Demirci <mdemirci@outlook.com>
2018-02-07 10:38:18 +0300

    projects page added

--- ProjectManager.Web/src/app/theme/pages/default/projects/projects.component.ts
+++ ProjectManager.Web/src/app/theme/pages/default/projects/projects.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { DataTableComponent } from '../components/datatable/datatable.component';
+import { Component, OnInit, ViewEncapsulation, AfterViewInit } from '@angular/core';
+import { Helpers } from '../../../../helpers';
+import { ScriptLoaderService } from '../../../../_services/script-loader.service';
 
 
 @Component({
-    selector: 'projects',
-    templateUrl: './projects.component.html',
+    selector: "projects",
+    templateUrl: "./projects.component.html",
     encapsulation: ViewEncapsulation.None,
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, AfterViewInit {
 
-    constructor() {
+
+    constructor(private _script: ScriptLoaderService) {
+
     }
-
     ngOnInit() {
+
     }
+    ngAfterViewInit() {
+        this._script.loadScripts('projects',
+            ['assets/demo/default/custom/components/datatables/base/data-ajax.js']);
+
+    }
+
 }
--- ProjectManager.Web/src/app/theme/pages/default/projects/projects.module.ts
+++ ProjectManager.Web/src/app/theme/pages/default/projects/projects.module.ts
@@ -1,33 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectsComponent } from './projects.component';
 import { LayoutModule } from '../../../layouts/layout.module';
 import { DefaultComponent } from '../default.component';
-import { ProjectsComponent } from './projects.component';
-import { DataTableComponent } from '../components/datatable/datatable.component';
 
 const routes: Routes = [
     {
-        'path': '',
-        'component': DataTableComponent,
-        'children': [
+        "path": "",
+        "component": DefaultComponent,
+        "children": [
             {
-                'path': '',
-                'component': ProjectsComponent,
-            },
-        ],
-    },
+                "path": "",
+                "component": ProjectsComponent
+            }
+        ]
+    }
 ];
-
 @NgModule({
     imports: [
-       CommonModule, RouterModule.forChild(routes), LayoutModule,
+        CommonModule, RouterModule.forChild(routes), LayoutModule
     ], exports: [
-        RouterModule,
+        RouterModule
     ], declarations: [
-        ProjectsComponent,
-        DefaultComponent
-    ],
+        ProjectsComponent
+    ]
 })
 export class ProjectsModule {
+
+
+
 }
